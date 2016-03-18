ed01cc59285ada2801a2afe6acd332955721d30c
mattgstevens <mattgstevens@gmail.com>
2016-03-18 20:59:42 +0100

    chore: catchup package.json

---
+++ src/utils/transaction.js
@@ -0,0 +1,4 @@
+var bitcoin = require('bitcoinjs-lib');
+
+// on payment we need to split: some to content owner, transaction fee, miner fee
+// https://medium.com/@orweinberger/how-to-create-a-raw-transaction-using-bitcoinjs-lib-1347a502a3a#.cnmdux4ig
