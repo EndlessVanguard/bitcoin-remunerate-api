cef20f092a89c96f8c2c26f15191260e97fb887b
mattgstevens <mattgstevens@gmail.com>
2016-03-20 00:11:31 +0100

    feat: transaction.addressPaidWithinTimeRange

--- src/utils/transaction.js
+++ src/utils/transaction.js
@@ -1,28 +1,34 @@
 const redisDb = require('../config/redis')
 
-function payout(contentId, paymentList, startTimestamp, stopTimestamp) {
-  return paymentList.filter(record => {
-    return record.contentId === contentId &&
-      record.paymentTimestamp > startTimestamp &&
-      record.paymentTimestamp < stopTimestamp
+function addressesPaidWithinTimeRange (contentId, paymentList, startTimestamp, stopTimestamp) {
+  // function that returns a list of all pubkeys successfully paid inbetween startTimestamp & stopTimestamp
+  return paymentList.filter((record) => {
+    if (record.contentId === contentId) { return true }
+
+    if (record.payment) {
+      return record.payment.timestamp > startTimestamp &&
+             record.payment.timestamp < stopTimestamp
+    }
+    return false
   })
 }
 
 function fetch (startTimestamp, stopTimestamp, contentId) {
   return new Promise((resolve, reject) => {
     redisDb.get('*', (err, data) => {
-      if(err) reject(err)
+      if (err) reject(err)
       resolve(data)
     })
   })
 }
 
 function makePayment (startTimestamp, stopTimestamp, contentId) {
-  return fetch(date.lastweek, Date.now(), "my-cool-shit")
-    .then((payments) => payout(contentId, payments, startTimestamp, stopTimestamp))
+  const date = { lastweek: Date.now() - 1 } // TODO(ms): make one week later
+  return fetch(date.lastweek, Date.now(), 'my-cool-shit')
+    .then((payments) => addressesPaidWithinTimeRange(contentId, payments, startTimestamp, stopTimestamp))
 }
 
 module.exports = {
-  payout: payout,
-  makePayment: makePayment,
+  addressesPaidWithinTimeRange: addressesPaidWithinTimeRange,
+  makePayment: makePayment
 }
--- src/utils/transaction.spec.js
+++ src/utils/transaction.spec.js
@@ -1,32 +1,35 @@
 const test = require('tape')
 
-require
-
 const transaction = require('./transaction')
-test('transaction.payout', (t) => {
+test('transaction.addressesPaidWithinTimeRange', (t) => {
   t.plan(1)
 
   const mockContentId = '123abc'
   const mockData = [
     {
       contentId: mockContentId,
-      paymentTimestamp: Date.now(),
+      payment: {
+        amount: 500000,
+        timestamp: Date.now()
+      }
     },
     {
       contentId: mockContentId,
-      paymentTimestamp: Date.now(),
+      payment: {
+        amount: 50000,
+        timestamp: Date.now()
+      }
     },
     {
-      contentId: mockContentId + 'nope',
-      paymentTimestamp: Date.now(),
-    },
+      contentId: mockContentId + 'nope'
+    }
   ]
   const timestamps = {
     start: (Date.now() - 1),
-    stop: (Date.now() + 1),
+    stop: (Date.now() + 1)
   }
 
-  const actual = transaction.payout(mockContentId, mockData, timestamps.start, timestamps.stop).length;
+  const actual = transaction.addressesPaidWithinTimeRange(mockContentId, mockData, timestamps.start, timestamps.stop).length
   const exepected = 2
   t.equal(actual, exepected)
 })
