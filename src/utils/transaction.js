0f56601d272272115f1e890f66c2abba42d3b0bf
mattgstevens <mattgstevens@gmail.com>
2016-03-20 15:22:42 +0100

    feat: transaction can build and broadcast a transaction with appropriate amounts to [payout, service, miner]

--- src/utils/transaction.js
+++ src/utils/transaction.js
@@ -21,7 +21,52 @@ function fetchContentAddresses (startTimestamp, stopTimestamp, contentId) {
   })
 }
 
-function makePayment (startTimestamp, stopTimestamp, contentId) {
+function calculateFee(total) {
+  // TODO: is 1% a fair way to calculate the miner's fee?
+  return {
+    payout: (total * 0.9),
+    service: (total * 0.09),
+    miner: (total * 0.01)
+  }
+}
+
+function buildTransaction(inputsList, payoutAddress, serviceAddress) {
+  const bitcoin = require('bitcoinjs-lib')
+  var tx = new bitcoin.TransactionBuilder()
+
+  inputsList.forEach((input, index) => {
+    var keyPair = bitcoin.ECPair.fromWIF(input.privateKey)
+    tx.addInput(input.lastTransaction, index)
+  })
+
+  const amount = calculateFee(
+    inputsList.reduce((sum, x) => sum + x.finalBalance, 0)
+  )
+
+  tx.addOutput(payoutAddress, amount.payout)
+  tx.addOutput(serviceAddress, amount.service)
+
+  inputsList.forEach((input, index) => {
+    var keyPair = bitcoin.ECPair.fromWIF(input.privateKey)
+    tx.sign(index, keyPair)
+  })
+  return tx.build().toHex()
+}
+
+function broadcastTransaction(transactionHex) {
+  const request = require('request')
+  return new Promise((resolve, reject) => {
+    request.post({
+      url: 'https://blockchain.info/pushtx',
+      form: { tx: transactionHex }
+    }, (err, res, body) => {
+      if(err) { reject(err) }
+      resolve(body)
+    })
+  })
+}
+
+function payoutContent (startTimestamp, stopTimestamp, contentId) {
   const date = { lastweek: Date.now() - 1 } // TODO(ms): make one week later
   return fetchContentAddresses(date.lastweek, Date.now(), 'my-cool-shit')
     .then((payments) => addressesPaidWithinTimeRange(contentId, payments, startTimestamp, stopTimestamp))
@@ -29,5 +74,8 @@ function makePayment (startTimestamp, stopTimestamp, contentId) {
 
 module.exports = {
   addressesPaidWithinTimeRange: addressesPaidWithinTimeRange,
-  makePayment: makePayment
+  buildTransaction: buildTransaction,
+  broadcastTransaction: broadcastTransaction,
+  calculateFee: calculateFee,
+  payoutContent: payoutContent
 }
