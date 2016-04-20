243844369db26a57ca41590083fa20b46560169f
Martin Josefsson <hello@martinjosefsson.com>
2016-04-20 11:46:19 +0200

    Major de-duplication of examples in records & isValid spec

--- src/records/Content.spec.js
+++ src/records/Content.spec.js
@@ -1,78 +1,49 @@
 const test = require('tape')
 const Content = require('./Content')
 const isEqual = require('lodash/isEqual')
+const R = require('ramda')
+
+const validContent = {
+  contentId: 'my-cool-content',
+  content: 'This is a cool article about something interesting',
+  price: 10000,
+  currency: 'satoshi',
+  payoutAddress: '19qwUC4AgoqpPFHfyZ5tBD279WLsMAnUBw'
+}
 
 test('isValidContent', (t) => {
   t.assert(
-    Content.isValidContent({
-      contentId: 'my-cool-content',
-      content: 'This is a cool article about something interesting',
-      price: 10000,
-      currency: 'satoshi',
-      payoutAddress: '19qwUC4AgoqpPFHfyZ5tBD279WLsMAnUBw'
-    }),
+    Content.isValidContent(validContent),
     'Valid Content record is valid')
   t.assert(
-    !Content.isValidContent({
-      content: 'This is a cool article about something interesting',
-      price: 10000,
-      currency: 'satoshi',
-      payoutAddress: '19qwUC4AgoqpPFHfyZ5tBD279WLsMAnUBw'
-    }),
+    !Content.isValidContent(R.dissoc('contentId', validContent)),
     'Content is missing contentId is invalid')
   t.assert(
-    !Content.isValidContent({
-      contentId: 'my-cool-content',
-      price: 10000,
-      currency: 'satoshi',
-      payoutAddress: '19qwUC4AgoqpPFHfyZ5tBD279WLsMAnUBw'
-    }),
+    !Content.isValidContent(R.dissoc('content', validContent)),
     'Content is missing content is invalid')
   t.assert(
-    !Content.isValidContent({
-      contentId: 'my-cool-content',
-      content: 'This is a cool article about something interesting',
-      price: '10000',
-      currency: 'satoshi',
-      payoutAddress: '19qwUC4AgoqpPFHfyZ5tBD279WLsMAnUBw'
-    }),
+    !Content.isValidContent(R.assoc('price', '10000', validContent)),
     'Content price can not be string')
   t.end()
 })
 
 test('errorsInContent', (t) => {
-  t.assert(
-    isEqual(
-      Content.errorsInContent({
-        contentId: 'my-cool-content',
-        content: 'This is a cool article about something interesting',
-        price: 10000,
-        currency: 'satoshi',
-        payoutAddress: '19qwUC4AgoqpPFHfyZ5tBD279WLsMAnUBw'
-      }),
-      []
-    ), 'Valid content contains no errors'
-  )
+  t.assert(isEqual(
+    Content.errorsInContent(validContent),
+    []
+  ), 'Valid content contains no errors')
 
   t.equal(
-    Content.errorsInContent({
-      contentId: 'my-cool-content',
-      content: 'This is a cool article about something interesting',
-      price: 10000,
-      payoutAddress: '19qwUC4AgoqpPFHfyZ5tBD279WLsMAnUBw'
-    }).length,
+    Content.errorsInContent(R.dissoc('currency', validContent)).length,
     1,
-    'Valid content contains no errors'
-  )
+    'Content missing currency gives an error')
 
   t.equal(
-    Content.errorsInContent({
-      contentId: 'my-cool-content',
-      content: 'This is a cool article about something interesting',
-      payoutAddress: '19qwUC4AgoqpPFHfyZ5tBD279WLsMAnUBw'
-    }).length,
+    // Wish Rambda dissoc would accept more than one key to dissoc...
+    Content.errorsInContent(R.dissoc('price',
+                                     R.dissoc('currency', validContent))).length,
     2,
-    'Valid content contains no errors')
+    'Content missing price and currency has 2 errors')
 
   t.end()
 })
--- src/utils/isValid.spec.js
+++ src/utils/isValid.spec.js
@@ -1,8 +1,10 @@
 const test = require('tape')
+const R = require('ramda')
 const isValid = require('./isValid')
 
 const isEqual = require('lodash/isEqual')
 const validates = require('../utils/validates.js')
+
 const mockProperties = {
   contentId: validates.errorsInString,
   content: validates.errorsInString,
@@ -11,50 +13,49 @@ const mockProperties = {
   payoutAddress: validates.errorsInBitcoinAddress
 }
 
+const validRecord = {
+  contentId: 'my-cool-content',
+  content: 'This is a cool article about something interesting',
+  price: 10000,
+  currency: 'satoshi',
+  payoutAddress: '19qwUC4AgoqpPFHfyZ5tBD279WLsMAnUBw'
+}
+
 test('isValidRecord', (t) => {
-  t.assert(isValid.isValidRecord({
-    contentId: 'my-cool-content',
-    content: 'This is a cool article about something interesting',
-    price: 10000,
-    currency: 'satoshi',
-    payoutAddress: '19qwUC4AgoqpPFHfyZ5tBD279WLsMAnUBw'
-  }, mockProperties))
+  t.assert(isValid.isValidRecord(validRecord, mockProperties))
+  t.assert(!isValid.isValidRecord(R.dissoc('payoutAddress', validRecord), mockProperties))
   t.end()
 })
 
 test('errorsInRecord', (t) => {
   t.assert(
     isEqual(
-      isValid.errorsInRecord({
-        contentId: 'my-cool-content',
-        content: 'This is a cool article about something interesting',
-        price: 10000,
-        currency: 'satoshi',
-        payoutAddress: '19qwUC4AgoqpPFHfyZ5tBD279WLsMAnUBw'
-      }, mockProperties),
+      isValid.errorsInRecord(validRecord, mockProperties),
       []
     ), 'Valid content contains no errors'
   )
 
   t.equal(
-    isValid.errorsInRecord({
-      contentId: 'my-cool-content',
-      content: 'This is a cool article about something interesting',
-      price: 10000,
-      payoutAddress: '19qwUC4AgoqpPFHfyZ5tBD279WLsMAnUBw'
-    }, mockProperties).length,
+    isValid.errorsInRecord(R.dissoc('currency', validRecord), mockProperties).length,
     1,
-    'Valid content contains no errors'
+    'Removing required field in Record gives errors'
   )
 
-  t.equal(
-    isValid.errorsInRecord({
-      contentId: 'my-cool-content',
-      content: 'This is a cool article about something interesting',
-      payoutAddress: '19qwUC4AgoqpPFHfyZ5tBD279WLsMAnUBw'
-    }, mockProperties).length,
-    2,
-    'Valid content contains no errors'
+  console.log(isValid.errorsInRecord(
+    R.dissoc('currency', R.dissoc('payoutAddress', validRecord)),
+    mockProperties))
+
+  t.assert(
+    R.equals(
+      isValid.errorsInRecord(
+        R.dissoc('currency', R.dissoc('payoutAddress', validRecord)),
+        mockProperties
+      ),
+      ['Currency must be "satoshi"',
+       'Bitcoin Address must begin with a "1" or "3"',
+       'Bitcoin Address must be a String']
+    ),
+    'Removing many required fields yields multiple errors'
   )
 
   t.end()
