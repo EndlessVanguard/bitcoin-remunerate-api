759a91a17f193ed8e9ceb0210b9adefd588b3fd2
Martin Josefsson <hello@martinjosefsson.com>
2016-05-03 16:24:24 +0100

    Invoice validations doesn't throw when lacking privateKey

--- src/records/Invoice.spec.js
+++ src/records/Invoice.spec.js
@@ -1,25 +1,23 @@
 const test = require('tape')
 const Invoice = require('./Invoice')
 const isEqual = require('lodash/isEqual')
+const dissoc = require('lodash/fp/dissoc')
+
+const validInvoice = {
+  createdAt: 1462287817,
+  address: '19qwUC4AgoqpPFHfyZ5tBD279WLsMAnUBw',
+  contentId: 'my-cool-content',
+  privateKey: 'L1dHE6RmNw345p2wy5m6dzyULAzqM96HdeHrfAKgU5sLYrNYpup9'
+}
 
 test('errorsInInvoice', (t) => {
-  t.assert(
-    isEqual(
-      Invoice.errorsInInvoice({
-        address: '19qwUC4AgoqpPFHfyZ5tBD279WLsMAnUBw',
-        contentId: 'my-cool-content',
-        privateKey: 'L1dHE6RmNw345p2wy5m6dzyULAzqM96HdeHrfAKgU5sLYrNYpup9'
-      }),
-      []
-    ), 'Valid content contains no errors'
-  )
+  t.assert(isEqual(Invoice.errorsInInvoice(validInvoice), []),
+           'Valid content contains no errors')
 
-  t.assert(
-    !Invoice.isValidInvoice({
-      address: '19qwUC4AgoqpPFHfyZ5tBD279WLsMAnUBw',
-      privateKey: 'L1dHE6RmNw345p2wy5m6dzyULAzqM96HdeHrfAKgU5sLYrNYpup9'
-    }),
-    'Invalid invoice is not valid'
-  )
+  t.assert(!Invoice.isValidInvoice(dissoc(validInvoice, 'privateKey')),
+           'Invoice without privateKey is invalid')
+  t.assert(!Invoice.isValidInvoice(dissoc(validInvoice, 'createdAt')),
+           'Invoice without createdAt timestamp is invalid')
+
   t.end()
 })
--- src/utils/validates.js
+++ src/utils/validates.js
@@ -1,6 +1,7 @@
 const isNumber = require('lodash/isNumber')
 const isString = require('lodash/isString')
 const isUndefined = require('lodash/isUndefined')
+const size = require('lodash/size')
 
 const errorsInBitcoinAddress = (address) => {
   var err = []
@@ -8,8 +9,8 @@ const errorsInBitcoinAddress = (address) => {
   if (!isString(address)) {
     err.push('Bitcoin Address must be a String')
   } else {
-    if (address.length < 26) { err.push('Bitcoin Address can not be shorter than 26 chars') }
-    if (address.length > 35) { err.push('Bitcoin Address can not be longer than 35 chars') }
+    if (size(address) < 26) { err.push('Bitcoin Address can not be shorter than 26 chars') }
+    if (size(address) > 35) { err.push('Bitcoin Address can not be longer than 35 chars') }
   }
   return err
 }
@@ -18,8 +19,8 @@ const errorsInPrivateKey = (privateKey) => {
   var err = []
   if (!isString(privateKey)) { err.push('must be a String') }
   if (!(/^[5KL]/).test(privateKey)) { err.push('must begin with one of: 5, K, L]') }
-  if (privateKey.length < 51) { err.push('Private Key WIF must be at least 51 characters long') }
-  if (privateKey.length > 52) { err.push('Private Key WIF can not be longer than 52 characters') }
+  if (size(privateKey) < 51) { err.push('Private Key WIF must be at least 51 characters long') }
+  if (size(privateKey) > 52) { err.push('Private Key WIF can not be longer than 52 characters') }
   return err
 }
 
@@ -35,7 +36,7 @@ const errorsInInteger = (int) => isInteger(int) ? [] : ['must be a Integer']
 const errorsInLabel = (label) => {
   var err = []
   if (!isString(label)) { err.push('No label given') }
-  if (isString(label) && label.length < 1) { err.push('No label given') }
+  if (isString(label) && size(label) < 1) { err.push('No label given') }
   return err
 }
 
